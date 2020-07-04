8d29b4475eb45bcf24be48099193946048a3a722
Abdullah Naveed <abdullah5naveed@gmail.com>
2020-07-04 15:02:49 +0500

    site is ready just have to add bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Blogs(models.Model):
+    blog_title = models.CharField(max_length=200)
+    blog_date = models.DateField(auto_now=False) 
+    blog_description = models.TextField()
+
+
+
+    def __str__(self):
+        return self.blog_title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blogs
 
-# Create your views here.
+def all_blog(request):
+    blogs = Blogs.objects.order_by('-blog_date')
+
+    return render(request, 'blog/all_blog.html', {'blogs':blogs})
+
+
+def details(request, blog_id):
+    blog = get_object_or_404(Blogs, pk=blog_id)
+    return render(request, 'blog/details.html', {'blog':blog})
